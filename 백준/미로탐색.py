ae991f51a99dc901ff42a81ba1a6e71ff39cdc36
wjd47 <wjd4782@naver.com>
2023-07-29 21:05:29 +0900

    2023-07-29

---
+++ 백준/미로탐색.py
@@ -0,0 +1,66 @@
+from collections import deque
+
+n, m = map(int, input().split())
+board = [input() for _ in range(n)]
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+chk = [[False] * m for _ in range(n)]
+
+def is_vaild(sx, sy):
+    return 0 <= sx < n and 0 <= sy < m
+
+
+dq = deque()
+dq.append((0, 0, 1))
+chk[0][0] = True
+    
+while len(dq) > 0:
+    x, y, d = dq.popleft()
+        
+    if x == n - 1 and y == m - 1:
+        print(d)
+        break
+        
+    for i in range(4):
+        sx = dx[i] + x
+        sy = dy[i] + y
+        sd = d + 1
+            
+        if is_vaild(sx, sy) and not chk[sx][sy] and board[sx][sy] == '1':
+            chk[sx][sy] == True
+            dq.append((sx, sy, sd))
+            
+# 정답 코드
+from collections import deque
+
+dy = (-1, 1, 0, 0)
+dx = (0, 0, 1, -1) # 좌우상하
+
+n, m = map(int, input().split())
+board = [input() for _ in range(n)]
+chk = [[False] * m for _ in range(n)]
+dq = deque()
+dq.append((0, 0, 1))
+chk[0][0] = True
+
+def is_valid(y, x):
+    return 0 <= y < n and 0 <= x < m
+
+while len(dq) > 0:
+    y, x, d = dq.popleft()
+    
+    if (y == n - 1 and x == m - 1):
+        print(d)
+        break      
+    
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
+        nd = d +  1
+        
+        if is_valid(ny, nx) and board[ny][nx] == '1' and not chk[ny][nx]:
+            chk[ny][nx] = True
+            dq.append((ny, nx, nd))
+                
+
+
