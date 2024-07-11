0f5a8d82a4ac7975a4c2d0bea9c732558ff826c6
wjd47 <wjd4782@naver.com>
2024-07-11 20:43:14 +0900

    2024-07-11

--- java/src/BJ_15989.java
+++ java/src/BJ_15989.java
@@ -3,25 +3,19 @@ import java.util.Scanner;
 public class BJ_15989 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
-        int[] dp;
-        for (int t = 0; t < test; t++) {
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
-            int idx = 3;
-            int cnt = 0;
-            dp = new int[10001];
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 3;
-            for (int i = 4; i <= n; i++) {
-                ++cnt;
-                dp[i] = dp[i - 3] + idx;
-                if (cnt == 2) {
-                    cnt = 0;
-                    idx++;
-                }
+            int[][] dp = new int[n + 1][4];
+
+            for (int j = 1; j <= n; j++) {
+                if (j >= 1) dp[j][1] = 1;
+                if (j >= 2) dp[j][2] = (j == 2) ? 1 : dp[j - 2][1] + dp[j - 2][2];
+                if (j >= 3) dp[j][3] = (j == 3) ? 1 : dp[j - 3][1] + dp[j - 3][2] + dp[j - 3][3];
             }
-            System.out.println(dp[n]);
+
+            System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
         }
+        sc.close();
     }
 }
