ba92c9889bc3905f50b654980ecf244244ddccb4
wjd47 <wjd4782@naver.com>
2024-08-06 21:10:46 +0900

    2024-08-06

--- java/src/BJ_11659.java
+++ java/src/BJ_11659.java
@@ -1,19 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BJ_11659 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] sumNums = new int[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            int num = sc.nextInt();
-            sumNums[i] = sumNums[i - 1] + num;
+
+    static int N, M, s, e;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken()); // 합배열 만들기
         }
-        for (int i = 0; i < m; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            System.out.println(sumNums[e] - sumNums[s - 1]);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            System.out.println(arr[e] - arr[s - 1]);    // 구간 합
         }
     }
 }
