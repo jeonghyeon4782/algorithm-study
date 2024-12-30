ec2e08e4e0eafcf12c37e9151768498b067a5c36
wjd47 <wjd4782@naver.com>
2024-12-30 23:20:19 +0900

    2024-12-30

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,45 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-public class Main {
+class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
 
-    static int n, r;
-    static int[] input;
-    static int[] nums;
-    static int[] isSelected;
+        ListIterator<Integer> iterator = list.listIterator();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        nums = new int[n];
-        isSelected = new int[n];
-        r = Integer.parseInt(st.nextToken());
-        input = new int[r];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        comb(0);
-    }
-
-    private static void comb(int cnt) {
-        if (cnt == n) {
-            System.out.println(Arrays.toString(isSelected));
-            return;
+        while(iterator.hasNext()) {
+            int i = iterator.next();
+            if (i == 2) iterator.remove();
+            if (i == 3) iterator.add(10);
         }
 
-        isSelected[cnt] = 1;
-        comb(cnt + 1);
-        isSelected[cnt] = 2;
-        comb(cnt + 1);
-        isSelected[cnt] = 3;
-        comb(cnt + 1);
-        isSelected[cnt] = 4;
-        comb(cnt + 1);
+        System.out.println(list);
     }
 }
