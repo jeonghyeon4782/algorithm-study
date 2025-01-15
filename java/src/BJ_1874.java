8f1bd5d7be87c61a1226272f7e6fb584f4d48c81
wjd47 <wjd4782@naver.com>
2025-01-15 21:11:41 +0900

    BJ_1874 2025-01-15

--- java/src/BJ_1874.java
+++ java/src/BJ_1874.java
@@ -1,45 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class BJ_1874 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        List<Character> answer = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
         int idx = 0;
-        char[] answer = new char[n * 2];
-        int answerIndex = 0;
-        boolean chk = true;
-        Stack<Integer> st = new Stack<>();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
 
             if (num > idx) {
-                for (int j = idx + 1; j < num + 1; j++) {
-                    st.push(j);
-                    answer[answerIndex++] = '+';
+                for (int j = idx + 1; j <= num; j++) {
+                    stack.push(j);
+                    answer.add('+');
                 }
+                stack.pop();
+                answer.add('-');
                 idx = num;
-                st.pop();
-                answer[answerIndex++] = '-';
             } else {
-                int popNum = st.pop();
-                if (popNum == num) {
-                    answer[answerIndex++] = '-';
+                if (stack.isEmpty() || stack.peek() != num) {
+                    System.out.println("NO");
+                    return;
                 } else {
-                    chk = false;
-                    break;
+                    stack.pop();
+                    answer.add('-');
                 }
             }
         }
-        if (chk) {
-            for (char c : answer) {
-                System.out.println(c);
-            }
-        } else {
-            System.out.println("NO");
+        for (char c : answer) {
+            System.out.println(c);
         }
     }
 }
