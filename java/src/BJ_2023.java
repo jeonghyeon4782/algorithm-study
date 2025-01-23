561b2c0e691433d8efcea3e1a6c38278182b9aaa
wjd47 <wjd4782@naver.com>
2025-01-23 16:08:08 +0900

    2025-01-23 BJ_2023

--- java/src/BJ_2023.java
+++ java/src/BJ_2023.java
@@ -2,45 +2,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BJ_2023 {
-    static ArrayList<Integer> nums;
-    static int n;
 
-    // 소수 체크
-    static boolean chk(int n) {
-        for (int i = 2; i < n - 1; i++) {
-            if (n % i == 0) {
-                return false;
-            }
+    static StringBuilder answer;
+    static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        answer = new StringBuilder();
+        for (int i = 1; i <= 9; i++) {
+            dfs(new StringBuilder(String.valueOf(i)), 1);
+        }
+        System.out.println(answer.toString());
+    }
+
+    private static void dfs(StringBuilder now, int depth) {
+        if (!isValid(now)) return;
+        if (depth == N) {
+            answer.append(now).append("\n");
+            return;
+        }
+        for (int i = 1; i <= 9; i++) {
+            dfs(now.append(String.valueOf(i)), depth + 1);
+            now.deleteCharAt(now.length() - 1);
+        }
+    }
+
+    private static boolean isValid(StringBuilder sb) {
+        int num = Integer.parseInt(sb.toString());
+        if (num == 1) return false;
+        for (int i = 2; i <= (int)Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
         }
         return true;
-    }
-
-    // dfs
-    static void DFS(String v) {
-        int num = Integer.parseInt(v);
-        if (!chk(num)) {
-            return;
-        }
-        if (v.length() == n) {
-            nums.add(num);
-            return;
-        }
-        for (int i = 0; i < 10; i++) {
-            DFS(v + String.valueOf(i));
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        nums = new ArrayList<>();
-        for (int i = 2; i < 10; i++) {
-            DFS(String.valueOf(i));
-        }
-        for (int ans : nums) {
-            System.out.println(ans);
-        }
     }
 }
