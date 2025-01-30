c037d70466af46d34ed0a33c8a910f7bf15f4197
wjd47 <wjd4782@naver.com>
2025-01-30 20:14:58 +0900

    2025-01-30 BJ_16234

--- java/src/BJ_16234.java
+++ java/src/BJ_16234.java
@@ -1,30 +1,32 @@
-package com.problem.BOJ;
-
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BJ_16234 {
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static int N, L, R;
+	static class Point {
+		int r, c;
+
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
+
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static int N, L, R, total;
 	static int[][] map;
 	static boolean[][] visited;
-	static List<Point> contryList;
-	static boolean flag;
+	static List<Point> moveList;
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
+		int answer = 0;
 		map = new int[N][N];
 
 		for (int i = 0; i < N; i++) {
@@ -34,69 +36,62 @@ public class BJ_16234 {
 			}
 		}
 
-		int result = 0;
 		while (true) {
-			flag = false;
 			visited = new boolean[N][N];
+			boolean flag = false;
 
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (!visited[i][j]) {
-						bfs(new Point(i, j));
-						move();
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if (!visited[r][c]) {
+						total = map[r][c];
+						moveList = new ArrayList<>();
+						moveList.add(new Point(r, c));
+						visited[r][c] = true;
+						bfs(new Point(r, c));
+						if (moveList.size() > 1) {
+							movePeople();
+							flag = true;
+						}
 					}
 				}
 			}
 
-			if (!flag) {
-				System.out.println(result);
-				break;
-			}
-
-			result++;
+			if (!flag) break;
+			++answer;
 		}
 
+		System.out.println(answer);
 	}
 
-	// 인구 이동
-	private static void move() {
-		int population = 0;
-		for (Point p : contryList) {
-			population += map[p.x][p.y];
-		}
-		population = (int) population / contryList.size();
-		for (Point p : contryList) {
-			map[p.x][p.y] = population;
+	private static void movePeople() {
+		int newPeo = total / moveList.size();
+		for (Point p : moveList) {
+			map[p.r][p.c] = newPeo;
 		}
 	}
 
-	public static void bfs(Point point) {
-		Queue<Point> q = new LinkedList<Point>();
-		contryList = new ArrayList<Point>(); // 연합 국가 리스트
-		visited[point.x][point.y] = true;
-		contryList.add(point);
+	private static void bfs(Point point) {
+		Queue<Point> q = new LinkedList<>();
 		q.offer(point);
 
 		while (!q.isEmpty()) {
 			Point now = q.poll();
-
 			for (int d = 0; d < 4; d++) {
-				int nr = now.x + dr[d];
-				int nc = now.y + dc[d];
+				int nr = now.r + dr[d];
+				int nc = now.c + dc[d];
 
-				if (isValid(nr, nc) && !visited[nr][nc] && Math.abs(map[now.x][now.y] - map[nr][nc]) >= L
-						&& Math.abs(map[now.x][now.y] - map[nr][nc]) <= R) {
-					flag = true;
+				if (isValid(nr, nc) && !visited[nr][nc] && L <= Math.abs(map[now.r][now.c] - map[nr][nc]) && Math.abs(map[now.r][now.c] - map[nr][nc]) <= R) {
+					q.offer(new Point(nr, nc));
+					moveList.add(new Point(nr, nc));
 					visited[nr][nc] = true;
-					Point np = new Point(nr, nc);
-					contryList.add(np);
-					q.offer(np);
+					total += map[nr][nc];
 				}
 			}
 		}
 	}
 
-	public static boolean isValid(int r, int c) {
+	private static boolean isValid(int r, int c) {
 		return 0 <= r && r < N && 0 <= c && c < N;
 	}
 }
+
