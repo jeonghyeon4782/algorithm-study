73c1981bf54f96d59e337dbb4233255ec035b9f7
wjd47 <wjd4782@naver.com>
2025-02-01 17:54:10 +0900

    2025-02-01

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,18 +1,23 @@
-import java.util.*;
+import java.util.Arrays;
 
-class Main {
+public class Main {
+    static int[] input = new int[10];
+    static int N = 5;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] map = new int[n];
-        int answer = 0;
-        String s = sc.next();
-        for (int i = 0; i < n; i++) {
-            map[i] = s.charAt(i) - '0';
+        comb(0, N);
+    }
+
+    private static void comb(int cnt, int arrow) {
+        if (cnt == 9) {
+            input[cnt] = arrow;
+            System.out.println(Arrays.toString(input));
+            return;
         }
-        for (int num : map) {
-            answer += num;
+
+        for (int i = 0; i <= arrow; i++) {
+            input[cnt] = i;
+            comb(cnt + 1, arrow - i);
         }
-        System.out.println(answer);
     }
 }
