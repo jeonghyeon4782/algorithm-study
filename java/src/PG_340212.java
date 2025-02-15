f4684632fd2f5c0d3614d2614df0a93a54c21321
wjd47 <wjd4782@naver.com>
2025-02-15 14:48:41 +0900

    2025-02-15 프로그래머스_340212 퍼즐 게임 챌리진

--- java/src/PG_340212.java
+++ java/src/PG_340212.java
@@ -1,51 +1,28 @@
-import java.util.*;
-
 class PG_340212 {
     public int solution(int[] diffs, int[] times, long limit) {
-        int answer = Integer.MAX_VALUE;
-        int s = Arrays.stream(diffs).min().orElse(Integer.MIN_VALUE);
-        int e = Arrays.stream(diffs).max().orElse(Integer.MAX_VALUE);
+        int answer = 0;
+        int s = 1;
+        int e = 0;
+        for (int diff : diffs) {
+            e = Math.max(e, diff);
+        }
 
         while (s <= e) {
-            long sum = 0;
-            int m = (s + e) / 2;
+            int m = (s + e) / 2;    // 현재 레벨
+            long allTime = 0;
 
-            for(int i = 0; i < diffs.length; i++) {
-                if(i == 0) {
-                    sum += solve(diffs[i], times[i], -1, limit, m);
+            for (int i = 0; i < diffs.length; i++) {
+                if (diffs[i] <= m) {
+                    allTime += times[i];
                 } else {
-                    sum += solve(diffs[i], times[i], times[i - 1], limit, m);
+                    allTime += (long)(diffs[i] - m) * (times[i] + times[i - 1]) + times[i];
                 }
             }
 
-            if(sum <= limit) {
-                e = m - 1;
-                answer = Math.min(answer, m);
-            } else {
-                s = m + 1;
-            }
+            if (allTime > limit) s = m + 1;
+            else e = m - 1;
         }
 
-        return answer;
-    }
-
-    public long solve(int diff, int time_cur, int time_prev, long limit, int level) {
-        long answer = 0;
-
-        if(time_prev == -1) {
-            if(diff <= level) {
-                answer += time_cur;
-            } else {
-                answer += (diff - level) * time_cur + time_cur;
-            }
-        } else {
-            if(diff <= level) {
-                answer += time_cur;
-            } else {
-                answer += (diff - level) * (time_cur + time_prev) + time_cur;
-            }
-        }
-
-        return answer;
+        return s;
     }
 }
