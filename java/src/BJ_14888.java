8d5f5ee16f4a8c5d6dff64b58b19b06190c33669
wjd47 <wjd4782@naver.com>
2025-04-25 17:32:29 +0900

    2025-04-25 백준_연산자 끼워넣기

--- java/src/BJ_14888.java
+++ java/src/BJ_14888.java
@@ -1,68 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class BJ_14888 {
-    static int N, maxNum, minNum;
-    static int[] nums, operators;
+    static int N;
+    static int[] nums;
+    static int[] op;
+    static int minNum = Integer.MAX_VALUE;
+    static int maxNum = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         nums = new int[N];
-        operators = new int[4];
+        op = new int[4];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        maxNum = Integer.MIN_VALUE;
-        minNum = Integer.MAX_VALUE;
-
         for (int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
-
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operators[i] = Integer.parseInt(st.nextToken());
+            op[i] = Integer.parseInt(st.nextToken());
         }
-
         dfs(0, nums[0]);
         System.out.println(maxNum);
         System.out.println(minNum);
     }
 
-    private static void dfs(int depth, int num) {
-
-        if (depth == N - 1) {
+    private static void dfs(int cnt, int num) {
+        if (cnt == N - 1) {
             minNum = Math.min(minNum, num);
             maxNum = Math.max(maxNum, num);
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-
-            if (operators[i] == 0) continue;
-            operators[i]--;
-
-            switch (i) {
-                case 0:
-                    dfs(depth + 1, num + nums[depth + 1]);
-                    operators[i]++;
-                    break;
-                case 1:
-                    dfs(depth + 1, num - nums[depth + 1]);
-                    operators[i]++;
-                    break;
-                case 2:
-                    dfs(depth + 1, num * nums[depth + 1]);
-                    operators[i]++;
-                    break;
-                case 3:
-                    dfs(depth + 1, num / nums[depth + 1]);
-                    operators[i]++;
-                    break;
-            }
+            if (op[i] == 0) continue;
+            op[i]--;
+            int newNum = cul(i, num, nums[cnt + 1]);
+            dfs(cnt + 1, newNum);
+            op[i]++;
         }
+    }
 
+    private static int cul(int op, int num1, int num2) {
+        switch (op) {
+            case 0:
+                return num1 + num2;
+            case 1:
+                return num1 - num2;
+            case 2:
+                return num1 * num2;
+            default:
+                if (num1 < 0) {
+                    num1 = -num1;
+                    return -(num1 / num2);
+                }
+                return num1 / num2;
+        }
     }
 }
