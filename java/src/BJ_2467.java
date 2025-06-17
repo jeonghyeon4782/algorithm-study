65e20c42468ff7138be62484c5925b0703783f2a
wjd47 <wjd4782@naver.com>
2025-06-17 15:57:25 +0900

    2025-06-17 백준_용액

--- java/src/BJ_2467.java
+++ java/src/BJ_2467.java
@@ -5,36 +5,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ_2467 {
+
+    static int N;
+    static int[] arr;
+    static int[] answer;
+
     public static void main(String[] args) throws IOException {
+        // 1. setting
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] nums = new int[N];
-        int[] answer = new int[2];
-        int min = Integer.MAX_VALUE;
+        StringTokenizer st = null;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        answer = new int[2];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 2. 투포인터
         int s = 0;
-        int e = N - 1;
-
+        int e = arr.length - 1;
+        int target = Integer.MAX_VALUE;
         while (s < e) {
-            int a = nums[s];
-            int b = nums[e];
-
-            if (Math.abs(a + b) <= min) {
-                answer[0] = a;
-                answer[1] = b;
-                min = Math.abs(a + b);
+            int sum = arr[s] + arr[e];
+            if (Math.abs(sum) <= target) {
+                answer[0] = arr[s];
+                answer[1] = arr[e];
+                target = Math.abs(sum);
             }
-
-            if (a + b <= 0) {
-                ++s;
-            } else {
+            if (sum >= 0) {
                 e--;
+            } else {
+                s++;
             }
         }
-        System.out.println(answer[0] + " " + answer[1]);
+
+        // 3. 정답 출력
+        Arrays.sort(answer);
+        StringBuilder sb = new StringBuilder();
+        sb.append(answer[0]).append(" ").append(answer[1]);
+        System.out.println(sb.toString());
     }
 }
